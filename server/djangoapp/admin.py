2e44b5681f090207a6e24226b736e98bf0e380d2
parth <parthmahapadi@gmail.com>
2024-02-05 07:50:18 -0500

    restapis

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-# from .models import related models
-from .models import *
+from .models import CarMake, CarModel
 
 # Register your models here.
+
+# CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
-    extra = 5
-
-class CarMakeAdmin(admin.ModelAdmin):
-    inlines = [CarModelInline]
-
-admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel)
-# CarModelInline class
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name',)
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,73 +1,52 @@
 from django.db import models
 from django.utils.timezone import now
 
-
-# Create your models here.
-
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
-    name = models.CharField(null= False, max_length=30, default='Audi')
-    description = models.CharField(null= False, max_length=300, default='Audi cars are aight')
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
 
     def __str__(self):
-        return 'Name:' + self.name + ',' + \
-            'Description:' + self.description
+        return self.name
 
-
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    SEDAN = 'sedan'
-    SUV = 'suv'
-    WAGON = 'wagon'
-    OTHERS = 'others'
-    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, 'SUV'), (WAGON, 'Wagon'), (OTHERS, 'Others')]
-    carmake = models.ForeignKey(CarMake, null= True, on_delete=models.CASCADE)
-    name = models.CharField(null= False, max_length=30, default='Audi X8')
-    dealerid = models.IntegerField(null=True)
-    cartype = models.CharField(null= False, max_length=20, choices= CAR_CHOICES, default=SEDAN)
-    year = models.DateField(null= True)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    type_c = models.CharField(max_length=10, choices=(('Sedan', 'Sedan',), ('SUV', 'SUV'), ('HATCHBACK', 'HATCHBACK'),('WAGON', 'WAGON')))
+    dealer_id = models.IntegerField()
+    year = models.DateField()
 
     def __str__(self):
-        return 'Name ' + self.name
+        return self.name
+
+
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
 class CarDealer:
-
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
-        # Dealer address
         self.address = address
-        # Dealer city
         self.city = city
-        # Dealer Full Name
         self.full_name = full_name
-        # Dealer id
         self.id = id
-        # Location lat
         self.lat = lat
-        # Location long
         self.long = long
-        # Dealer short name
         self.short_name = short_name
-        # Dealer state
         self.st = st
-        # Dealer zip
         self.zip = zip
-        self.idx = 0
 
     def __str__(self):
         return "Dealer name: " + self.full_name
 
-
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
-        self.name = name
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
         self.dealership = dealership
-        self.review = review
+        self.name = name
         self.purchase = purchase
+        self.review = review
         self.purchase_date = purchase_date
         self.car_make = car_make
         self.car_model = car_model
         self.car_year = car_year
         self.sentiment = sentiment
-
-    def __str__(self):
-        return "Review: " + self.review
+        self.id = id
